bfece855f6efed49bf7c8abf65917d0c0d754b62
tesaaufi-hub <tesaufi@gmail.com>
2025-10-10 23:50:43 +0700

    bagian awal

--- tugas1.cpp
+++ tugas1.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+using namespace std;
+
+void cekKelayakan(double BulananPerAnggota) {
+    cout << "\nRata-rata penghasilan per anggota: Rp " << BulananPerAnggota << endl;
+    if (BulananPerAnggota < 750000)
+        cout << "Diperbolehkan untuk mendaftar KIP.\n";
+    else
+        cout << "Tidak diperbolehkan untuk mendaftar KIP.\n";
+}
 
 int main() {
+    int jumlahAnggota;
+    cout << "Masukkan jumlah anggota keluarga: ";
+    cin >> jumlahAnggota;
+
+    int umur;
+    double penghasilan, totalPenghasilan = 0;
 
 
     return 0;
