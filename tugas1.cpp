873720df349a5d9823e9a5df5ba0db62089ae83e
rimazakiyya-beep <rimazakiyya@gmail.com>
2025-10-11 08:33:53 +0700

    yang baru

--- tugas1.cpp
+++ tugas1.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-int nol= 000;
+    int jumlahAnggota; // anggota yg berpenghasilan
+    int umur;
+    float penghasilan;
+    double pk_gabung; //pendapatan kotor gabungan anggota
+    char operasi;
+
+    
+    cout << "Apakah anda ada di usia produktif? " << endl;
+    cout << "Masukkan Umur: ";
+    cin >> umur ;
+
+    if (umur > 16 && umur < 65) { 
+        cout << "Produktif" << endl;
+        } else {
+        cout << "Tidak lolos syarat penerimaan" << endl;
+    }
+
+    cout << "Berapa orang yang berpenghasilan? ";
+    cin >> jumlahAnggota;
+    cout << "Berapa penghasilan per bulan? ";
+    cin >> penghasilan;
+
+    if (operasi == '*') {
+        cout << "Penghasilan kotor keluargamu: " << jumlahAnggota * penghasilan;
+    }
+
+    cout << "Penghasilan kotor keluargamu: ";
+    cin >> pk_gabung;
+
+    if (pk_gabung <= 4000000) {
+        cout << "Lolos syarat batas maksimum ekonomi";
+    } else {
+        cout << "Tidak lolos batas maksimum ekonomi";
+    }
+    
+
 
 
     return 0;
